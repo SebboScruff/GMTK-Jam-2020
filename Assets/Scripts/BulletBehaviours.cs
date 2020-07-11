7bc71c78570fd852bc4c07e28167b4d9010befdd
SebboScruff <36053779+SebboScruff@users.noreply.github.com>
2020-07-11 15:47:22 +0100

    some more progress on shooting

--- Assets/Scripts/BulletBehaviours.cs
+++ Assets/Scripts/BulletBehaviours.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class BulletBehaviours : MonoBehaviour
 {
+    public BulletType thisBulletType;
+    public float moveSpeed;
+    Vector3 movementVector;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        moveSpeed = 15f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch(thisBulletType)
+        {
+            case BulletType.FORWARD:
+                movementVector = Vector3.up;
+                break;
+            case BulletType.LEFT:
+                movementVector = Vector3.left;
+                break;
+            case BulletType.RIGHT:
+                movementVector = Vector3.right;
+                break;
+            case BulletType.DUD:
+                movementVector = Vector3.zero;
+                break;
+            default:
+                break;
+        }
+
+        transform.position += movementVector * moveSpeed * Time.deltaTime;
     }
+
+
+}
+
+public enum BulletType
+{
+    FORWARD,
+    LEFT,
+    RIGHT,
+    DUD
 }
