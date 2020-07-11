7020b696af8eb7fd62954c5ce727bb972846a2dc
SebboScruff <36053779+SebboScruff@users.noreply.github.com>
2020-07-11 20:45:04 +0100

    made a bullet tag

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -2,17 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/* Pretty simple arcade-style
+ * enemy behaviours, downwards
+ * movement and then despawn
+ */
+
 public class EnemyBehaviour : MonoBehaviour
 {
+    public float moveSpeed;
+    public float shootingFrequency, shootingCooldown;
+
+    public Transform firingPoint;
+    public GameObject bullet;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        shootingFrequency = 2f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+    }
+
+    void Shoot()
+    {
+        Instantiate(bullet, firingPoint.position, firingPoint.rotation);
+        shootingCooldown = shootingFrequency;
+        InvokeRepeating("ShootingCooldown", 1f, 1f);
+    }
+
+    void ShootingCooldown()
+    {
+        if(shootingCooldown > 0)
+        {
+            shootingCooldown -= 1f;
+        }
+        else { CancelInvoke(); }
     }
 }
